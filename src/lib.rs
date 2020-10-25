406ed31a19dba1a5deb75b00a663827942bf4400
Vaelint <freylint@tuta.io>
2020-10-25 05:07:58 -0500

    Add integration test.

--- src/lib.rs
+++ src/lib.rs
@@ -15,17 +15,17 @@ pub mod prelude {
 }
 
 /// Bevy plugin for easily adding eminput to a bevy app
-pub struct InputPlugin;
+pub struct EMInputPlugin;
 
-impl Plugin for InputPlugin {
+impl Plugin for EMInputPlugin {
     fn build(&self, app: &mut AppBuilder) {
         #[cfg(feature = "1DInput")]
-            app.add_system_to_stage(
+        app.add_system_to_stage(
             stage::PRE_UPDATE,
             InputUpdateSystem1D::update_input_1d.system(),
         );
         #[cfg(feature = "2DInput")]
-            app.add_system_to_stage(
+        app.add_system_to_stage(
             stage::PRE_UPDATE,
             InputUpdateSystem2D::update_input_2d.system(),
         );
@@ -36,8 +36,26 @@ impl Plugin for InputPlugin {
 // TODO benches
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    #[ignore]
+    /// Tests a bevy app by adding input plugin and running it once
+    /// NOTE requires running in single threaded test mode
+    /// ie: cargo test -- --test-threads 1 --ignored
+    /// This requirement is why it is ignored by default
+    // TODO Setup integration tests to run this as a seperate
+    // invocation of cargo test
+    fn test_bevy_2dinput() {
+        App::build()
+        .add_plugin(bevy::input::InputPlugin)
+        .add_plugin(bevy::app::ScheduleRunnerPlugin::run_once())
+        .add_plugin(EMInputPlugin)
+        .run();
     }
 }
